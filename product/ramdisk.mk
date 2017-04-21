83c67232281a0168566bc0cb6b8bf760dcd2c07a
Ntemis <ierokipides.dem@gmail.com>
2017-04-21 15:48:03 +0300

    Add ramdisk and ril pkg

---
+++ product/ramdisk.mk
@@ -0,0 +1,12 @@
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.qcom.usb.rc \
+    init.qcom.bt.sh \
+    init.qcom.power.rc \
+    init.qcom.rc \
+    init.qcom.sh \
+    init.qcom-ims.rc \
+    init.qcom.mem.sh \
+    init.qti.ims.sh \
+    ueventd.qcom.rc

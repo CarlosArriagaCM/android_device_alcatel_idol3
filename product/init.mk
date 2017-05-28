189c3e53f01a8e1c7ead7d24b2f8d85fc0d5bb6b
FireLord1 <firelord.xda@gmail.com>
2017-05-28 15:40:27 +0530

    idol3: Make sure we copy rootdir files
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- product/init.mk
+++ product/init.mk
@@ -3,6 +3,9 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.mem.sh \
     init.qcom.ril.sh \
+    init.qcom.bt.sh \
+    init.tct.variant.sh \
     init.qcom.power.rc \
     init.qcom.rc \
+    init.qcom.usb.rc \
     ueventd.qcom.rc

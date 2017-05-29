98b1b5d3735ad639b803974464c5cac05ec15589
Ntemis <ierokipides.dem@gmail.com>
2017-05-29 16:19:40 +0300

    Idol3:clean&fix board packages

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,4 +1,12 @@
-BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw \
-    device/alcatel/idol3/cmhw
+# CMHW
 BOARD_USES_CYANOGEN_HARDWARE := true
+BOARD_HARDWARE_CLASS += \
+    $(LOCAL_PATH)/cmhw \
+    hardware/cyanogen/cmhw
+
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QC_TIME_SERVICES := true
+TARGET_POWERHAL_VARIANT := qcom
+TARGET_TAP_TO_WAKE_NODE := "/sys/class/tp_gesture/tp_device/tp_gesture_id"
+TARGET_USE_SDCLANG := true

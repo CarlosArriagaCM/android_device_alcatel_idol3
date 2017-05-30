a06bc5e8ba0e5664aff640fb38fa1433de49c66a
ntemis <ierokipides.dem@gmail.com>
2017-05-30 22:29:53 +0300

    Add missing libs removed by mistake

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -15,3 +15,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
 endif
+
+PRODUCT_PACKAGES += \
+        librmnetctl \
+        libxml2

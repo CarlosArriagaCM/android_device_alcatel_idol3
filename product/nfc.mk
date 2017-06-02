0ed542e1305ab10df4cc529e89fc8573bbffe2b2
Ntemis <ierokipides.dem@gmail.com>
2017-06-02 22:35:07 +0300

    Add back default nfc config
    Change-Id: Ib954ee2594f28d11f78348afbdec3cb142447303

--- product/nfc.mk
+++ product/nfc.mk
@@ -1,5 +1,6 @@
 # NFC
 PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
     nfc_nci.bcm2079x.default \
     NfcNci \
     Tag
@@ -9,4 +10,5 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     device/alcatel/idol3/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     device/alcatel/idol3/configs/nfc/libnfc-brcm-20795a10.conf:system/etc/libnfc-brcm-20795a10.conf \
-    device/alcatel/idol3/configs/nfc/libnfc-brcm-20795a20.conf:system/etc/libnfc-brcm-20795a20.conf
+    device/alcatel/idol3/configs/nfc/libnfc-brcm-20795a20.conf:system/etc/libnfc-brcm-20795a20.conf \
+    device/alcatel/idol3/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml

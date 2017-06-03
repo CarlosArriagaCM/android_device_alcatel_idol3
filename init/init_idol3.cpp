acc897e2cfb3855b606360bd08a44a8136549b95
Ntemis <ierokipides.dem@gmail.com>
2017-06-03 15:30:07 +0300

    Fix init model name and add led pulsating capability

--- init/init_idol3.cpp
+++ init/init_idol3.cpp
@@ -117,28 +117,28 @@ void vendor_load_properties()
 
     std::string curef_version = property_get("ro.cm.curef");
 
-    if (curef_version.find("6045I") == 0) {
+    if (curef_version == "6045I") {
         /* 6045I (North America) */
         common_properties();
         gsm_properties("9");
         property_set("ro.build.fingerprint", "TCL/6045I/idol3:6.0.1/MMB29M/v7VAB-0:user/release-keys");
         property_set("ro.build.description", "idol3-user 6.0.1 MMB29M v7VAB-0 release-keys");
         property_set("ro.product.model", "6045I");
-    } else if (curef_version.find("6045B") == 0) {
+    } else if (curef_version == "6045B") {
         /* 6045B */
         common_properties();
         gsm_properties("9");
         property_set("ro.build.fingerprint", "TCL/6045B/idol3:5.0.2/LRX22G/v7SQX-0:user/release-keys");
         property_set("ro.build.description", "idol3-user 5.0.2 LRX22G v7SQX-0 release-keys");
         property_set("ro.product.model", "6045B");
-     } else if (curef_version.find("6045F") == 0) {
+     } else if (curef_version == "6045F") {
         /* 6045F */
         common_properties();
         gsm_properties("10");
         property_set("ro.build.fingerprint", "TCL/6045F/idol3:5.0.2/LRX22G/v7TM4-0:user/release-keys");
         property_set("ro.build.description", "idol3-user 5.0.2 LRX22G v7TM4-0 release-keys");
         property_set("ro.product.model", "6045F");
-    } else if (curef_version.find("6045K") == 0) {
+    } else if (curef_version == "6045K") {
         /* 6045K */
         common_properties();
         gsm_properties("9,1");
@@ -146,14 +146,14 @@ void vendor_load_properties()
         property_set("ro.build.fingerprint", "TCL/6045K/idol3:6.0.1/MMB29M/v7VG6-0:user/release-keys");
         property_set("ro.build.description", "idol3-user 6.0.1 MMB29M v7VG6-0 release-keys");
         property_set("ro.product.model", "6045K");
-    } else if (curef_version.find("6045O") == 0) {
+    } else if (curef_version == "6045O") {
         /* 6045O Cricket */
         common_properties();
         gsm_properties("9");
         property_set("ro.build.fingerprint", "TCL/6045O/idol3:5.0.2/LRX22G/v5AMB:user/release-keys");
         property_set("ro.build.description", "idol3-user 5.0.2 LRX22G v5AMB release-keys");
         property_set("ro.product.model", "6045O");
-    } else if (curef_version.find("6045Y") == 0) {
+    } else if (curef_version == "6045Y") {
         /* 6045Y */
         common_properties();
         gsm_properties("9");
@@ -161,10 +161,10 @@ void vendor_load_properties()
         property_set("ro.build.description", "idol3-user 6.0.1 MMB29M v7VG6-0 release-keys");
         property_set("ro.product.model", "6045Y");
     } else {
-       /* lolwtfbbq */
+       /* Default to TCL IDOL3 */
        common_properties();
        gsm_properties("9");
-       property_set("ro.product.model", "lolwtfbbq");
+       property_set("ro.product.model", "TCL IDOL3");
     }
 
         std::string model = property_get("ro.product.model");

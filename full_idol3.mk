3708ed7d59d43eaf3cb3291bf9d32a023470877e
Ntemis <ierokipides.dem@gmail.com>
2017-06-04 19:47:21 +0300

    Update id name from stock

--- full_idol3.mk
+++ full_idol3.mk
@@ -25,6 +25,6 @@ $(call inherit-product, device/alcatel/idol3/device.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := idol3
 PRODUCT_NAME := full_idol3
-PRODUCT_BRAND := Alcatel
-PRODUCT_MODEL := Idol3
+PRODUCT_BRAND := TCL
+PRODUCT_MODEL := idol3
 PRODUCT_MANUFACTURER := TCL

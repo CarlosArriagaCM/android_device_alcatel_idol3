c02486d11b11dd66572686c742127e2893c12693
Ntemis <ierokipides.dem@gmail.com>
2017-06-16 18:10:25 +0300

    Fix camera camcorder also enable Google camera's Photo Sphere,  Lens Blur, and Panorama features.

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -10,6 +10,6 @@ PRODUCT_COPY_FILES += \
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.cpp.duplication=false \
-    persist.camera.hal.debug.mask=0
-
-# TARGET_CAMERASERVICE_CLOSES_NATIVE_HANDLES := true
+    persist.camera.hal.debug.mask=0 \
+    camera2.portability.force_api=1 \
+    media.stagefright.legacyencoder=true

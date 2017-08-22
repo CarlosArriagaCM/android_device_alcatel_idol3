2e55bd7e29af96294d7fc832d8c8e06532133d04
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:03:01 +0200

    idol3: Update display/render for O
    * Add various HALs
    * Enable HWC2 & Gralloc1

--- product/display.mk
+++ product/display.mk
@@ -12,10 +12,24 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
     gralloc.msm8916 \
     copybit.msm8916 \
     hwcomposer.msm8916 \
-    memtrack.msm8916
+    memtrack.msm8916 \
+    libgenlock
+
+# QMI
+PRODUCT_PACKAGES += \
+    libtinyxml
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
 
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)

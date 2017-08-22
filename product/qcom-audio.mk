d8a5ccbbf9ef91412575a3ae2c2a8e4e905eb518
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:07:43 +0200

    idol3: Update audio for O
    * Build audio HALs
    * Remove dead packages
    * Update audio flags for AOSP HAL

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,9 +1,20 @@
 # Audio
 PRODUCT_PACKAGES += \
-    audiod \
+    audio.primary.msm8916 \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default
+
+PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
@@ -15,4 +26,5 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml

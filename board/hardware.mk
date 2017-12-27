7b28fe1c864d8b4be721022786f82cead5e77735
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-27 23:28:13 +0530

    idol3: memory: Enable SVELTE memory configuration

--- board/hardware.mk
+++ board/hardware.mk
@@ -9,3 +9,4 @@ BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QC_TIME_SERVICES := true
 TARGET_POWERHAL_VARIANT := qcom
 TARGET_TAP_TO_WAKE_NODE := "/sys/class/tp_gesture/tp_device/tp_gesture_id"
+MALLOC_SVELTE := true

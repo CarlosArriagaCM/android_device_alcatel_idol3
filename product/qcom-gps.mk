0423616d7e2b2e0df2ed48486085fdaec7744ed1
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-29 00:37:27 +0530

    idol3: Build GNSS HAL

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -10,6 +10,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/xtwifi.conf:system/etc/xtwifi.conf
 
+# GNSS HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml

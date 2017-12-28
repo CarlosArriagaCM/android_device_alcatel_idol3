a364e60b4ba27dc106006eee7485abadba64b18c
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-28 15:57:16 -0600

    idol3: removed duplicated flag

--- board/platform.mk
+++ board/platform.mk
@@ -11,6 +11,3 @@ TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8916

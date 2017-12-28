e472a6c3316db52e06a00a1f69941acdbc5ae546
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-28 15:45:06 -0600

    idol3: removed TARGET_KERNEL_APPEND_DTB
    * deprecated

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,14 +1,13 @@
+# Platform
+TARGET_BOARD_PLATFORM := msm8916
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_SOURCE := kernel/alcatel/msm8916
 TARGET_KERNEL_CONFIG := lineage_idol3_defconfig
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8916

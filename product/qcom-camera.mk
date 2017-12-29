6591c67786ea5168cac01b4ed74af3e0d28d8a5a
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-29 22:54:02 +0530

    idol3: build camera OSS

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,7 +1,10 @@
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    Snap
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
+    camera.msm8916 \
+    libmm-qcamera \
+    Camera2
 
 # Permissions
 PRODUCT_COPY_FILES += \

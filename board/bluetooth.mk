4b318f6b4b2a15435243a1ed6e9ea208ede2764d
Carlos Arriaga <carlosarriagacm@gmail.com>
2018-01-01 11:51:21 -0600

    idol3: update bluetooth configs for O

--- board/bluetooth.mk
+++ board/bluetooth.mk
@@ -1,2 +1,6 @@
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/alcatel/idol3/bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+QCOM_BT_USE_BTNV := true

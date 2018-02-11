d9dc462812348356c81a04e133bebe79280dc28e
carlosarriagacm <carlosarriagacm@gmail.com>
2018-02-11 20:21:22 +0000

    Revert "idol3: Build camera OSS"
    This reverts commit 61384d9930bd4a50776cc0a099f37bd7e9c23282.

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,10 +1,7 @@
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@3.2-impl \
-    camera.msm8916 \
-    libmm-qcamera \
-    Snap
+    android.hardware.camera.provider@2.4-impl-legacy \
+    Camera2
 
 # Permissions
 PRODUCT_COPY_FILES += \

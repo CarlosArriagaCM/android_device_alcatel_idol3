9c767a4c0c353a77d44c9d0c8509d42b9f4c1f00
carlosarriagacm <carlosarriagacm@gmail.com>
2018-02-11 20:16:34 +0000

    Revert "idol3: dummy configs"
    This reverts commit 6c8d6ac973aeae529260d7932ed095f1f3794468.

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,17 +1,16 @@
 # Audio
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
-    audio.usb.default \
-    audio.r_submix.default \
-    libqcompostprocbundle \
-    libqcomvisualizer \
-    libqcomvoiceprocessing
-
-PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    audiod \
+    audio.a2dp.default \
+    audio.r_submix.default \
+    audio.usb.default \
+    libaudio-resampler \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libqcompostprocbundle \
+    tinymix
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \

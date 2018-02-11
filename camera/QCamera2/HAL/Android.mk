e0ccb7b2dcb3de437788023e23dbf85a745ba456
carlosarriagacm <carlosarriagacm@gmail.com>
2018-02-11 20:21:06 +0000

    Revert "idol3: camera: fix build in 8.1"
    This reverts commit 8e2b0dc54321fad0045354fea144385cefa38426.

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -53,9 +53,6 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
         frameworks/native/include \
         frameworks/native/include/media/openmax \
-        frameworks/native/libs/nativewindow/include \
-        frameworks/native/libs/nativebase/include \
-        frameworks/native/libs/arect/include \
         $(call project-path-for,qcom-display)/libgralloc \
         $(call project-path-for,qcom-display)/libqdutils \
         $(call project-path-for,qcom-media)/libstagefrighthw \
